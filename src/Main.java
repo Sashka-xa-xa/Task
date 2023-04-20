f8e186d36707dbafb3b5ff94bd501f232f0f5ef7
Asash <sasha.kurbatova.88@mail.ru>
2023-04-20 15:20:23 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double ticketCost = 20000;
+        int milesEarned = (int) (ticketCost / 20);
+        System.out.println("Заработано миль: " + milesEarned);
+
+    }
+}
